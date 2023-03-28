d8a697b5925a8902f32fc64a29536dd535e61eab
Baxket <richardablorh7@gmial.com>
2023-03-28 08:37:31 -0700

    main

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
 #include <stdarg.h>
-#include <stdio.h>
 #include <unistd.h>
 
 #define UNUSED(x) (void)(x)
@@ -43,7 +42,6 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -114,4 +112,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif
